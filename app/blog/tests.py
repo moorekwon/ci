73e2656feb18258974310a9ae9833cbabfa7670e
Hyojin Kwon <raccoonhj33@gmail.com>
2020-03-13 19:52:19 +0900

    200313 total test coverage 100%

--- app/blog/tests.py
+++ app/blog/tests.py
@@ -1,3 +1,7 @@
+# Create your tests here.
 from django.test import TestCase
 
-# Create your tests here.
+
+class BlogTest(TestCase):
+    def test_post_list(self):
+        response = self.client.get('/posts/')
